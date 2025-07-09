2af90d30d756ff5b175133e4bb5317d7d54861ee
suzuv063 <fbornhofen@uv.uni-kiel.de>
2025-07-09 15:05:13 +0200

    fizzbuzz

---
+++ fizzbuzz/script.js
@@ -0,0 +1,15 @@
+let o
+
+for (let i = 1; i <= 1; i++) {
+    if (i % 3 == 0) and (i % 5 == 0); {
+        o= "fizzbuzz";
+    } else if (i % 3 == 0) {
+        o = "fizz";
+    } else if (i % 5 == 0) {
+        o = "buzz";
+    } else { 
+        o = i;
+    }
+
+    console.log(`${o}`)
+}
