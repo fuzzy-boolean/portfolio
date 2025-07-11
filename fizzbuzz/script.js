e37e1c3debec4363ab16b9e9e00e2a127f4c8eac
suzuv063 <fbornhofen@uv.uni-kiel.de>
2025-07-11 09:49:54 +0200

    now?

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,6 +1,5 @@
 let o;
 
-<<<<<<< HEAD
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         o = "fizzbuzz";
@@ -13,18 +12,3 @@ for (let i = 1; i <= 100; i++) {
     }
 
     console.log(`${o}`);
-=======
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    o = "fizzbuzz";
-  } else if (i % 3 == 0) {
-    o = "fizz";
-  } else if (i % 5 == 0) {
-    o = "buzz";
-  } else {
-    o = i;
-  }
-
-  console.log(`${o}`);
->>>>>>> 1be7409aec672e9af4a1ee6e4d810342a6dd2490
-}
